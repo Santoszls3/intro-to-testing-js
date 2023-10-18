563a4f8f0bdf4ee98086d18aa70ab572b25b69fd
Santos Gonzales <Santoszls3@gmail.com>
2023-10-18 16:40:08 -0500

    addeds codetest intro

--- code.js
+++ code.js
@@ -1,4 +1,19 @@
 // helloWorld function 
-function helloWorld() {
+
+
+//                                      original:
+//function helloWorld() {
+//    return "Hello, World!";
+//}
+
+
+//Now, go to code.js and replace the function statement for helloWorld with
+// a function expression. Do all the tests stay green or not? Why or why not?
+// Double check your syntax. These are interchangeable because functions are
+// first class citizens in the JS language.
+const helloWorld = function() {
     return "Hello, World!";
+}
+function sayHello(){
+
 }
